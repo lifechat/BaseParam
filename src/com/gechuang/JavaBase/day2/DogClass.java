c08412886ff83c89a3a25318daae1651ba62831c
xiaqi17 <2596642641@qq.com>
2020-10-14 07:18:56 +0800

    定义类

---
+++ src/com/gechuang/JavaBase/day2/DogClass.java
@@ -0,0 +1,20 @@
+/**
+    *实现数据增加操作
+     * @param vo 表示要执行操作的对象
+     * @return 成功返回true，失败返回false
+     *  @throws SQLException
+    * */
+package com.gechuang.JavaBase.day2;
+
+/**  
+*    
+* 项目名称：BaseParam   
+* 类名称：DogClass   
+* 类描述：   
+* 创建人：25966   
+* 创建时间：2020年10月14日 上午7:18:14   
+* @version        
+*/
+public class DogClass {
+
+}
